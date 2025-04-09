ed62a842ef179b2d45dbe681f89097ca6cf3d5a9
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-04-09 23:35:02 +0530

    Add files via upload

---
+++ React - BroCode - StyleReactComponentsWithCSS/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+//How to style React Components with CSS
+//-------------------------------------
+//(not including external frameworks or preprocessors)
+
+// 1.External
+// 2.Modules
+// 3.Inline
+
+import Button from "./assets/Button"
+
+
+function App() {
+   
+  return (
+    <Button/>
+  );
+  
+}
+
+export default App
---
+++ React - BroCode - StyleReactComponentsWithCSS/vite-project/src/assets/Button/Button.jsx
@@ -0,0 +1,8 @@
+import styles from "./Button.module.css"
+
+function Button(){
+     return(
+        <button className="button">Click Me</button>
+     );
+}
+export default Button
